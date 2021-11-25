279da273e4ac65c9c5e1f3e0222b88e1abd5410f
kevenallan <kevenallan98@gmail.com>
2021-11-25 17:12:33 -0300

    rpc-rabbitmq server/cliente

---
+++ rpc-rabbitmq/src/main/java/br/com/ifpb/pdist/rpcrabbitmq/RpcRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.pdist.rpcrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpcRabbitmqApplication.class, args);
+	}
+
+}
---
+++ rpc-rabbitmq/src/test/java/br/com/ifpb/pdist/rpcrabbitmq/RpcRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.pdist.rpcrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
