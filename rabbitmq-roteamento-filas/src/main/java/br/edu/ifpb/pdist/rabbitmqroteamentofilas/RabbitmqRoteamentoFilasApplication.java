4c3df3570eb46620228e756839e998dfb24b0f65
kevenallan <kevenallan98@gmail.com>
2021-11-25 20:42:01 -0300

    rabbitmq roteamento-filas

---
+++ rabbitmq-roteamento-filas/src/main/java/br/edu/ifpb/pdist/rabbitmqroteamentofilas/RabbitmqRoteamentoFilasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pdist.rabbitmqroteamentofilas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqRoteamentoFilasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqRoteamentoFilasApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-roteamento-filas/src/test/java/br/edu/ifpb/pdist/rabbitmqroteamentofilas/RabbitmqRoteamentoFilasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pdist.rabbitmqroteamentofilas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqRoteamentoFilasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
