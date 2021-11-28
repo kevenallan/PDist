432093586315c47552a9aa38e94cc1276f79d554
kevenallan <kevenallan98@gmail.com>
2021-11-28 19:53:38 -0300

    rabbitmq, tentativa de utilizar workers, persistencia e acks

---
+++ rabbitmq-workes-persistencia-acks/src/main/java/br/edu/ifpb/pdist/rabbitmqworkespersistenciaacks/NewTask.java
@@ -0,0 +1,26 @@
+package br.edu.ifpb.pdist.rabbitmqworkespersistenciaacks;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTask {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            String message = String.join(" ", argv);
+
+            channel.basicPublish("", "olá",null,message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+
+}
---
+++ rabbitmq-workes-persistencia-acks/src/main/java/br/edu/ifpb/pdist/rabbitmqworkespersistenciaacks/RabbitmqWorkesPersistenciaAcksApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pdist.rabbitmqworkespersistenciaacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqWorkesPersistenciaAcksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqWorkesPersistenciaAcksApplication.class, args);
+	}
+
+}
